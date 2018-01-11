2ad48118f0dd608b35b2eb5e26e3acfddb98ea8c
Art Gillespie <art@tapsquare.com>
2018-01-10 18:14:42 -0800

    Firebase, Firebase Auth config + boilerplate

--- ios-photo-uploader/LegoImageUploader/LegoImageUploader/ViewController.swift
+++ ios-photo-uploader/LegoImageUploader/LegoImageUploader/ViewController.swift
@@ -7,12 +7,31 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
 
 class ViewController: UIViewController {
+    
+    var user: User?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let auth = Auth.auth()
+        if let u = auth.currentUser  {
+            print("Already signed in! \(u.providerData[0].email!)")
+            self.user = u
+        } else {
+            auth.signIn(withEmail: "<>", password: "<>", completion: { (u: User?, e: Error?) in
+                if let error = e {
+                    print("Error logging in! \(error)")
+                } else if let user = u {
+                    print("Logged in! \(user.providerData[0].email!)")
+                    self.user = user
+                } else {
+                    print("Both user and error are nil :-(")
+                }
+            })
+        }
     }
 
     override func didReceiveMemoryWarning() {
